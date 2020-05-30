17209ea1ce643aa7e8dfc6cececed2977b78c3af
onButtonUp <jtferrier3@msn.com>
2020-05-30 07:43:42 -0700

    added a function
    demo : )

--- src/Example200530d.jl
+++ src/Example200530d.jl
@@ -1,5 +1,8 @@
 module Example200530d
 
+include("extra_file.jl")
 # Write your package code here.
+
+export my_f
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using Example200530d
 using Test
 
+#Example200530d.my_f(2,1)
+
 @testset "Example200530d.jl" begin
-    # Write your tests here.
+    @test my_f(2,1) == 7
+    @test my_f(2,3) == 13
 end
