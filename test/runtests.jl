5460dce50f1e2db2ddb062c5358e5bf7daff5a36
onButtonUp <jtferrier3@msn.com>
2020-05-30 08:07:56 -0700

    my new test with breaks

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,8 @@
 using Example200530d
 using Test
 
-#Example200530d.my_f(2,1)
-
 @testset "Example200530d.jl" begin
     @test my_f(2,1) == 7
     @test my_f(2,3) == 13
+    @test my_f(2,3) == 11
 end
